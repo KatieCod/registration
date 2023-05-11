506d996015b640ac3dca33c2bcc992497200ba59
Katie <e.v.rassolenko@gmail.com>
2023-05-11 20:34:04 +0300

    all done

--- classes.js
+++ classes.js
@@ -1,5 +1,5 @@
 class User{
-  constructor(name='', email='', birthdate='', city='', street='', house='', image='', hobbies=[]){
+  constructor(name='', email='', birthdate='', city='', street='', house='', image='', hobbies=[], happy='', skydiving='', dollar=''){
     this.name=name
     this.email=email
     this.birthdate=birthdate
@@ -8,5 +8,9 @@ class User{
     this.house=house
     this.image=image
     this.hobbies=hobbies
+    this.happy=happy
+    this.skydiving=skydiving
+    this.dollar=dollar
+
   }
 }
